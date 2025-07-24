4aaae974be5621f41b5cfacd80ca4ee3b22c8cc2
sleepinglion <admin@jedaeroweb.co.kr>
2025-07-24 19:16:09 +0900

    수정함:        .gitignore 	수정함:        .idea/workspace.xml 	수정함:        app/views/locations/index.html.erb 	수정함:        config/deploy.rb 	수정함:        config/locales/ko.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,29 @@
-set :application, 'sleepinglion'
-set :repo_url, 'git@github.com:sleepinglion/pamphlet.git'
+set :application, "community"
+set :repo_url, "git@github.com:jedaeroweb/pamphlet.git"
 set :branch, 'master'
 set :deploy_to, '/home/deploy/pamphlet'
-# set :scm, :git
 
 # set :format, :pretty
 # set :log_level, :debug
-set :pty, true
-set :linked_files, %w{config/database.yml .env}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/assets public/ckeditor public/uploads}
+#set :pty, true
+set :linked_files, %w{config/database.yml config/master.key .env .ruby-version}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/assets public/uploads}
+
+set :rbenv_type, :user
+set :rbenv_ruby, "3.3.4"
+set :ssh_options, verify_host_key: :never
+set :default_env, {'NODE_OPTIONS'=>'--openssl-legacy-provider'}
+set :bundle_path, '/usr/local/bundle'
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       within release_path do
-         execute :rake, 'tmp:clear'
+        execute :rake, 'tmp:clear'
       end
     end
   end
