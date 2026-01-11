712f2cbfe033848604f305786565a2b3cb99713e
sleepinglion <admin@jedaeroweb.co.kr>
2026-01-11 15:41:44 +0900

    수정함:        config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module Pamphlet
     config.i18n.fallbacks = [:ko]
     config.i18n.enforce_available_locales = true
     # Configuration for the application, engines, and railties goes here.
+    config.assets.paths << Rails.root.join("app/assets/builds")
     config.assets.enabled = true
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
